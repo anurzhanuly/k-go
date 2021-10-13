70b14e91ffa5c134cd58193e04745a86de188465
azatbek <altynbekov@kolesa.kz>
2021-10-13 17:32:37 +0600

    Lecture 3

---
+++ client/client.go
@@ -0,0 +1,32 @@
+package client
+
+import (
+	"context"
+	"github.com/sirupsen/logrus"
+	pb "go.kolesa-team.org/gl/go-course/rpc"
+	"google.golang.org/grpc"
+)
+
+type Client struct {
+	Addr string
+}
+
+func NewClient(addr string) *Client {
+	return &Client{
+		Addr: addr,
+	}
+}
+
+func (c *Client) Client(ctx context.Context) (pb.ServiceClient, error) {
+	conn, err := grpc.DialContext(ctx, c.Addr, grpc.WithInsecure())
+
+	if err != nil {
+		logrus.WithError(err).Error("Фатальная ошибка при подключении клиента")
+
+		return nil, err
+	}
+
+	client := pb.NewServiceClient(conn)
+
+	return client, err
+}
